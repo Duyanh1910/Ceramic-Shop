1abec79062b3037d867a9b825320d9ebe6c7686b
DuyanhVMU <157499492+DuyanhVMU@users.noreply.github.com>
2026-03-24 17:27:26 +0700

    Cap nhat gmail

--- backend/config/mail.config.js
+++ backend/config/mail.config.js
@@ -4,8 +4,9 @@ const user = process.env.EMAIL_USERNAME;
 const password = process.env.EMAIL_PASSWORD;
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
+  
   auth: {
     user: user,
     pass: password,
