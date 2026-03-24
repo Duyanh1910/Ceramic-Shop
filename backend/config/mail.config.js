a4b4332cb9f6333cb3da7290fbc566a8ba96c108
DuyanhVMU <157499492+DuyanhVMU@users.noreply.github.com>
2026-03-24 17:39:32 +0700

    Email fix

--- backend/config/mail.config.js
+++ backend/config/mail.config.js
@@ -1,4 +1,6 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
 
 const user = process.env.EMAIL_USERNAME;
 const password = process.env.EMAIL_PASSWORD;
@@ -6,7 +8,6 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
-  
   auth: {
     user: user,
     pass: password,
