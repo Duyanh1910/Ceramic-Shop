167fd71f384909c221562b81327e6833b7faba18
DuyanhVMU <157499492+DuyanhVMU@users.noreply.github.com>
2026-04-01 13:56:43 +0700

    Bug cx chỉ là bọ

--- backend/routes/payment/vnpay.route.js
+++ backend/routes/payment/vnpay.route.js
@@ -3,9 +3,9 @@ import * as vnpayController from "../../controllers/payment/vnpay.controller.js"
 
 const router = express.Router();
 
-router.post("vnpay/create", vnpayController.createPayment);
-router.get("vnpay/return", vnpayController.vnpayReturn);
-router.get("vnpay/ipn", vnpayController.vnpayIpn);
-router.get("vnpay/check", vnpayController.checkPayment);
+router.post("/vnpay/create", vnpayController.createPayment);
+router.get("/vnpay/return", vnpayController.vnpayReturn);
+router.get("/vnpay/ipn", vnpayController.vnpayIpn);
+router.get("/vnpay/check", vnpayController.checkPayment);
 
 export default router;
