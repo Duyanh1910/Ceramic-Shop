80b02d6c75dd85ff4f29a98277a5a21bc70c825d
DuyanhVMU <157499492+DuyanhVMU@users.noreply.github.com>
2026-04-02 07:46:34 +0700

    vnpAy

--- backend/utils/payment/vnpay.util.js
+++ backend/utils/payment/vnpay.util.js
@@ -1,40 +1,12 @@
-import crypto from "crypto";
-import qs from "qs";
-
-export const sortObject = (obj) => {
-  const sorted = {};
-  const keys = Object.keys(obj).sort();
-
-  for (let key of keys) {
-    sorted[key] = encodeURIComponent(obj[key]).replace(/%20/g, "+");
-  }
-
-  return sorted;
-};
-
 export const createSecureHash = (params, secretKey) => {
-  const signData = qs.stringify(params, { encode: false });
+  const signData = Object.keys(params)
+    .map((key) => `${key}=${params[key]}`)
+    .join("&");
 
   return crypto
     .createHmac("sha512", secretKey)
     .update(signData, "utf-8")
     .digest("hex");
-};
-
-export const buildPaymentUrl = (params, config) => {
-  const vnpParams = {
-    vnp_Version: "2.1.0",
-    vnp_Command: "pay",
-    vnp_TmnCode: config.tmnCode,
-    ...params,
-  };
-
-  const sorted = sortObject(vnpParams);
-  const secureHash = createSecureHash(sorted, config.hashSecret);
-
-  sorted.vnp_SecureHash = secureHash;
-
-  return `${config.vnpUrl}?${qs.stringify(sorted, { encode: false })}`;
 };
 
 export const verifyVnpay = (query, secretKey) => {
@@ -45,8 +17,17 @@ export const verifyVnpay = (query, secretKey) => {
   delete vnpParams["vnp_SecureHash"];
   delete vnpParams["vnp_SecureHashType"];
 
+  Object.keys(vnpParams).forEach((key) => {
+    if (!key.startsWith("vnp_")) {
+      delete vnpParams[key];
+    }
+  });
+
   const sorted = sortObject(vnpParams);
-  const signData = qs.stringify(sorted, { encode: false });
+
+  const signData = Object.keys(sorted)
+    .map((key) => `${key}=${sorted[key]}`)
+    .join("&");
 
   const signed = crypto
     .createHmac("sha512", secretKey)
