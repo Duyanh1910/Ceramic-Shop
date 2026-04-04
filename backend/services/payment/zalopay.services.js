b7247c8fc6eb26b50589142d869c7e24b748bfa0
DuyanhVMU <157499492+DuyanhVMU@users.noreply.github.com>
2026-04-05 06:25:03 +0700

    sửa lỗi callback zalopay

--- backend/services/payment/zalopay.services.js
+++ backend/services/payment/zalopay.services.js
@@ -84,6 +84,7 @@ export const createZaloPayPaymentUrl = async (maDonHang) => {
       description: `Thanh toan don hang ${maDonHang}`,
       bank_code: "",
       mac: mac,
+      callback_url: "https://ceramic-shop-u8ak.onrender.com/api/v1/zalo-ipn",
     };
 
     try {
