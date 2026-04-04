10c1fc39216c1f25426bc0e4fe653273a60557f7
DuyanhVMU <157499492+DuyanhVMU@users.noreply.github.com>
2026-04-04 15:10:28 +0700

    Sửa lỗi zalopay

--- backend/routes/payment/momo.route.js
+++ backend/routes/payment/momo.route.js
@@ -3,8 +3,15 @@ import {
   createPayment,
   handleMoMoIpn,
 } from "../../controllers/payment/momo.controller.js";
-
+import {
+  createZaloPayPayment,
+  zaloPayCallback,
+} from "../../controllers/payment/zalopay.controller.js";
 const router = express.Router();
+
+router.post("/zalo-create", createZaloPayPayment);
+router.post("/zalo-ipn", zaloPayCallback);
+
 router.post("/momo-create", createPayment);
 router.post("/momo-ipn", handleMoMoIpn);
 
