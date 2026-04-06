1d7dc0c6baa0466417816bb29273ff900da3474e
Phap-1824 <vuquocphap10082004@gmail.com>
2026-04-07 00:39:35 +0700

    F7

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,12 +1,11 @@
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { Dropdown, Avatar, Space, Layout, Menu, Input, Select, Row, Col, Pagination, Spin, Badge, message, AutoComplete, Popover, Button, Radio, Rate } from 'antd';
-import { LogoutOutlined,UserOutlined, SearchOutlined, ShoppingCartOutlined, DeleteOutlined, ReloadOutlined, AppstoreOutlined, EyeOutlined, UserOutlined, ShoppingOutlined } from '@ant-design/icons';
+import { LogoutOutlined, SearchOutlined, ShoppingCartOutlined, DeleteOutlined, ReloadOutlined, AppstoreOutlined, EyeOutlined, UserOutlined, ShoppingOutlined } from '@ant-design/icons';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import styles from './Home.module.css';
 import { useAutoLogout, clearSession } from './useAuth.js';
-import { icons } from 'antd/es/image/PreviewGroup.js';
 
 const { Header, Sider, Content } = Layout;
 
