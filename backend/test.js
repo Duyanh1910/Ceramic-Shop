daa526a81e48f737adda6731668614db07c5f389
DuyanhVMU <157499492+DuyanhVMU@users.noreply.github.com>
2026-04-28 00:53:57 +0700

    test color

--- backend/test.js
+++ backend/test.js
@@ -1,4 +1,10 @@
-import { AccountModel, CustomerModel } from "./models/index.js";
+import {
+  AccountModel,
+  CustomerModel,
+  OrderModel,
+  OrderDetailModel,
+  
+} from "./models/index.js";
 import { Op } from "sequelize";
 const row = await CustomerModel.findOne({
   include: [
